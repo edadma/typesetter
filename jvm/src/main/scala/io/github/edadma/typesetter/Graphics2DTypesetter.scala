e16b608d8f75532db52d5bc58b017c651b83940a
Edward A Maxedon <edadma@gmail.com>
2024-10-22 20:54:19 -0400

    update

--- jvm/src/main/scala/io/github/edadma/typesetter/Graphics2DTypesetter.scala
+++ jvm/src/main/scala/io/github/edadma/typesetter/Graphics2DTypesetter.scala
@@ -1,27 +1,27 @@
 package io.github.edadma.typesetter
 
-import java.awt.font.TextLayout
+import java.awt.font.{FontRenderContext, TextLayout}
 import java.awt.image.BufferedImage
-import java.awt.{Graphics2D, RenderingHints, Toolkit, Font as JFont}
+import java.awt.{Graphics2D, RenderingHints, Font as JFont}
 import java.io.File
 import javax.imageio.ImageIO
 import scala.compiletime.uninitialized
 
 class Graphics2DTypesetter extends Typesetter:
 
-  var page: BufferedImage = uninitialized
-  var g: Graphics2D = uninitialized
+  private var page: BufferedImage = uninitialized
+  private var g: Graphics2D = uninitialized
+  private var frc: FontRenderContext = uninitialized
 
   g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
   g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
 
-  private val frc = g.getFontRenderContext
-
-  def init(): Unit =
+  def initTarget(): Unit =
     page = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB)
     g = page.createGraphics()
+    frc = g.getFontRenderContext
 
-  def render(box: Box, width: Double, height: Double, xoffset: Double = 0, yoffset: Double = 0): Any =
+  def createPageTarget(width: Double, height: Double): Any =
     page = new BufferedImage(
       width.toInt,
       height.toInt,
@@ -32,10 +32,22 @@ class Graphics2DTypesetter extends Typesetter:
     g.fillRect(0, 0, page.getWidth, page.getHeight)
     g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
     g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
-    box.draw(this, xoffset, yoffset + box.ascent)
     page
 
-  def getDPI: Double = Toolkit.getDefaultToolkit.getScreenResolution
+  def renderToTarget(box: Box, xoffset: Double = 0, yoffset: Double = 0): Unit =
+    box.draw(this, xoffset, yoffset + box.ascent)
+
+  def ejectPageTarget(): Unit = ()
+
+  /*
+  Precision-7530
+  --------------
+  xrandr | grep -w connected
+  eDP-1 connected primary 1920x1080+0+0 (normal left inverted right x axis y axis) 340mm x 190mm
+
+  DPI is 143.435294114
+   */
+  def getDPI: Double = 143.435294114 // Toolkit.getDefaultToolkit.getScreenResolution
 
   def setFont(font: Any): Unit = g.setFont(font.asInstanceOf[JFont])
 
@@ -52,8 +64,8 @@ class Graphics2DTypesetter extends Typesetter:
 
   def loadFont(path: String): JFont = java.awt.Font.createFont(java.awt.Font.TRUETYPE_FONT, new java.io.File(path))
 
-  def getTextExtents(text: String): TextExtents =
-    val layout = new TextLayout(text, currentFont.renderFont.asInstanceOf[JFont], frc)
+  def getTextExtents(text: String, font: Any): TextExtents =
+    val layout = new TextLayout(text, font.asInstanceOf[JFont], frc)
     val bounds = layout.getBounds
 
     val ascent = -bounds.getY
